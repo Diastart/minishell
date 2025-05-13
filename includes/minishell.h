3ede3e7c29c3d0297875465b122280d4109ad9b3
Dinursult <give.me.kitchen@gmail.com>
2025-05-13 17:20:03 +0200

    initial tests for readline function

---
+++ includes/minishell.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Dias <dinursul@student.42.it>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/13 17:08:30 by Dias              #+#    #+#             */
+/*   Updated: 2025/05/13 17:15:29 by Dias             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+# include <stdio.h>
+# include <stdlib.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+#endif
