4cf54d930f7204e13482386da0299cbdfd38d80a
Dinursult <give.me.kitchen@gmail.com>
2025-07-13 23:25:42 +0200

    heredoc redir handling is added which is creating subchild and wriring to the pipe and reading from there

--- core/routing.c
+++ core/routing.c
@@ -6,13 +6,52 @@
 /*   By: dias <dias@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/11 00:02:39 by dias              #+#    #+#             */
-/*   Updated: 2025/07/13 22:38:46 by dias             ###   ########.fr       */
+/*   Updated: 2025/07/13 23:24:18 by dias             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mini.h"
 
-static t_last	redirinout(t_cmd *lclcmd, t_last *lastredirs)
+
+static int handle_heredoc(char *delimiter)
+{
+	int		pipefd[2];
+	char	*line;
+	pid_t	pid;
+
+	if (pipe(pipefd) == -1)
+		return -1;
+	pid = fork();
+	if (pid == 0)
+	{
+		close(pipefd[0]);
+		while (1)
+		{
+			write(STDOUT_FILENO, "> ", 2);
+			line = readline("");
+			
+			if (!line || ft_strcmp(line, delimiter) == 0)
+			{
+				if (line) free(line);
+				break;
+			}
+			write(pipefd[1], line, ft_strlen(line));
+			write(pipefd[1], "\n", 1);
+			free(line);
+		}
+		
+		close(pipefd[1]);
+		exit(0);
+	}
+	else
+	{
+		close(pipefd[1]);
+		wait(NULL);
+		return pipefd[0];
+	}
+}
+
+static void	redirinout(t_cmd *lclcmd, t_last *lastredirs)
 {
 	int		fd;
 	t_redir	*lclredir;
@@ -32,16 +71,16 @@ static t_last	redirinout(t_cmd *lclcmd, t_last *lastredirs)
 		else if (lclredir->type == REDIR_HEREDOC)
 		{
 			lastredirs->delim = lclredir->filename;
-			fd = open(lclredir->filename, O_WRONLY | O_CREAT, 0777);
+			fd = handle_heredoc(lclredir->filename);
 			if (fd == -1)
 				exit(259);
-			dup2(fd, STDOUT_FILENO);
+			dup2(fd, STDIN_FILENO);
 			close(fd);
 		}
 		else if (lclredir->type == REDIR_OUT)
 		{
 			lastredirs->newline = OK;
-			fd = open(lclredir->filename, O_WRONLY | O_CREAT, 0777);
+			fd = open(lclredir->filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 			if (fd == -1)
 				exit(259);
 			dup2(fd, STDOUT_FILENO);
@@ -50,10 +89,10 @@ static t_last	redirinout(t_cmd *lclcmd, t_last *lastredirs)
 		else if (lclredir->type == REDIR_APPEND)
 		{
 			lastredirs->newline = KO;
-			fd = open(lclredir->filename, O_RDONLY, 0777);
+			fd = open(lclredir->filename, O_WRONLY | O_CREAT | O_APPEND, 0644);
 			if (fd == -1)
 				exit(259);
-			dup2(fd, STDIN_FILENO);
+			dup2(fd, STDOUT_FILENO);
 			close(fd);
 		}
 		lclredir = lclredir->next;
